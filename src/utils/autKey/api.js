7f515079aef331579a56086fd4a30f18876c2c64
AkashDY <89009592+AkashDY@users.noreply.github.com>
2023-12-05 13:10:30 +0530

    bugs fixed

--- src/utils/autKey/api.js
+++ src/utils/autKey/api.js
@@ -1,7 +1,7 @@
 export const authSingUp =
-  "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBoGo-7zM7XoxrDuPNFU2UqwrZK4gDwETQ";
+  "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBmz6w8yAZ_OhAVYHG7KxdWlXet5eTDqHU";
 export const authSignIn =
-  "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBoGo-7zM7XoxrDuPNFU2UqwrZK4gDwETQ";
+  "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBmz6w8yAZ_OhAVYHG7KxdWlXet5eTDqHU";
 export const baseAddress =
-  "https://mail-box-client-db989-default-rtdb.firebaseio.com/";
-export const authKey = "AIzaSyBoGo-7zM7XoxrDuPNFU2UqwrZK4gDwETQ";
+  "https://mail-box-client-app-32998-default-rtdb.firebaseio.com/";
+export const authKey = "AIzaSyBmz6w8yAZ_OhAVYHG7KxdWlXet5eTDqHU";
